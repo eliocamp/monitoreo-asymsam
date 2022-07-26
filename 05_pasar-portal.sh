666bbe63c0f61edc3f3bc821d30c1617760ec4bf
eliocamp <eliocampitelli@gmail.com>
2022-07-26 13:22:11 -0300

    Cambia a script de bash

---
+++ 05_pasar-portal.sh
@@ -0,0 +1,6 @@
+
+scp index.html elio.campitelli@portal.cima.fcen.uba.ar:~/wwwuser/asymsam/monitoreo/index.html
+
+scp -r index_files elio.campitelli@portal.cima.fcen.uba.ar:~/wwwuser/asymsam/monitoreo/index_files
+
+scp -r plots elio.campitelli@portal.cima.fcen.uba.ar:~/wwwuser/asymsam/monitoreo/plots
