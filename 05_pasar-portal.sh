68cf97c64d89df0567e306459c55d38160d589bf
eliocamp <eliocampitelli@gmail.com>
2022-07-28 11:25:13 -0300

    Agrega cosas al script

--- 05_pasar-portal.sh
+++ 05_pasar-portal.sh
@@ -1,4 +1,12 @@
 
+# Actualiza los datos
+Rscript 03_calcular_sam_ultimo-mes.R
+Rscript 04_generar-figuras.R
+
+# Actualiza el Rmd
+Rscript -e 'rmarkdown::render("index.Rmd")'
+
+# Mueve todo
 scp index.html elio.campitelli@portal.cima.fcen.uba.ar:~/wwwuser/asymsam/monitoreo/index.html
 
 scp -r index_files elio.campitelli@portal.cima.fcen.uba.ar:~/wwwuser/asymsam/monitoreo/index_files
